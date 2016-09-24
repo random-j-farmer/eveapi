4a6c895b90703cae4aa19e9d46a3f44bf6b4a17a
Random J. Farmer <random.j.farmer@gmail.com>
2016-09-24 21:14:22 +0200

    find systems/regions

--- mapdata/mapdata.go
+++ mapdata/mapdata.go
@@ -1,6 +1,7 @@
 package mapdata
 
 import "github.com/random-j-farmer/eveapi/types"
+import "strings"
 
 // SolarSystemByID gets the system info
 func SolarSystemByID(id uint64) types.SolarSystem {
@@ -24,4 +25,36 @@ func RegionNameByID(regionID uint64) string {
 // GatesBySolarSystemID gives a solar systems gates
 func GatesBySolarSystemID(solarSystemID uint64) []types.Gate {
 	return gatesBySolarSystemID[solarSystemID]
+}
+
+// NameAndID is a name/id pair for searching
+type NameAndID struct {
+	Name string
+	ID   uint64
+}
+
+// FindRegions finds regions by prefix
+func FindRegions(prefix string) []NameAndID {
+	prefix = strings.ToLower(prefix)
+	var result []NameAndID
+	for id, name := range regionNameByID {
+		if strings.HasPrefix(strings.ToLower(name), prefix) {
+			result = append(result, NameAndID{Name: name, ID: id})
+		}
+	}
+
+	return result
+}
+
+// FindSolarSystems finds systems by prefix
+func FindSolarSystems(prefix string) []NameAndID {
+	prefix = strings.ToLower(prefix)
+	var result []NameAndID
+	for id, system := range systemNameByID {
+		if strings.HasPrefix(strings.ToLower(system.SolarSystemName), prefix) {
+			result = append(result, NameAndID{Name: system.SolarSystemName, ID: id})
+		}
+	}
+
+	return result
 }
